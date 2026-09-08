62ff38549e4c245f051f553a2d3678b8b5158080
fobaty <fobaty8@gmail.com>
2026-09-08 14:46:16 -0700

    chore(release): bump version to v0.1.2

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub const VERSION: &str = "V0.1.1";
+pub const VERSION: &str = "V0.1.2";
 pub const AP_SSID_DEF: &str = "AIR-SCAN-CONFIG";
 pub const AP_PASS_DEF: &str = "12345678";
 pub const WIFI_TIMEOUT_MS: u64 = 10000;
